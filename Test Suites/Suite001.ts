0daf519612e96da7ed0876bb60b924e30e8e6135
shraddhach <shraddhach@SHRADDHACH.cybage.com>
2020-02-04 15:01:22 +0530

    created Suite001 added testDemo testcase inside suite001

---
+++ Test Suites/Suite001.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7a106c52-77d2-47c6-9eb7-a9150324bc0c</testSuiteGuid>
+</TestSuiteEntity>

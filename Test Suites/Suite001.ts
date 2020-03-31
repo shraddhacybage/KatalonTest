3979239d21232e13bc0a84896725e29cc6d2567f
shraddhach <shraddhach@SHRADDHACH.cybage.com>
2020-03-31 15:13:11 +0530

    deleted Wash web automation code

--- Test Suites/Suite001.ts
+++ Test Suites/Suite001.ts
@@ -10,4 +10,10 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>7a106c52-77d2-47c6-9eb7-a9150324bc0c</testSuiteGuid>
+   <testCaseLink>
+      <guid>89160966-7177-451a-a38c-254b907fd705</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestDemo</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
